fc6978fec752d7dc05af910b98f260b770f39ac1
Jane Lee <Yesdoing13@gmail.com>
2019-02-24 18:41:30 +0900

    TodoList with TypeScript + React

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import TodoList from './components/TodoList';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello React with TypeScript!</h1>
+        <TodoList />
       </div>
     );
   }
